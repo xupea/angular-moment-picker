a48370ed09f25d96de20521e69aff8ac95d66180
Peter Hsu <sysu.peter.hsu@gmail.com>
2017-10-19 15:07:30 +0800

    updates

--- example/app.js
+++ example/app.js
@@ -1,1 +1,7 @@
-angular.module('slinkapp', ['moment-picker'])
+angular.module('slinkapp', ['moment-picker']).controller('demoController', ['$scope', function() {
+  var ctrl = this;
+  ctrl.maxDateMoment = moment().add(1, 'day');
+  ctrl.maxDateString = moment().add(1, 'day').format('MMM YYYY');
+  ctrl.minDateString = moment().subtract(17, 'month').format('MMM YYYY');
+  ctrl.input = 'Select Month';
+}])
